44b3032dfcc59d7a50ec36f44950a4105a5edffa
Min Gyo Kim <mingyokim@Mins-MacBook-Pro.local>
2018-01-14 10:51:02 -0800

    link ajax function to button

--- scripts/azure.js
+++ scripts/azure.js
@@ -1,15 +1,17 @@
-$("button").click(function(){
-    $.ajax({
-        type: "POST",
-        url: "https://westus.api.cognitive.microsoft.com/vision/v1.0/analyze?visualFeatures=Categories&language=en",
-        data: {
-            "url": "https://www.petmd.com/sites/default/files/what-does-it-mean-when-cat-wags-tail.jpg",
-        },
-        success: function(result) {
-            alert('ok');
-        },
-        error: function(result) {
-            alert('error');
-        }
+$(document).ready(function(){
+    $("button").click(function(){
+        $.ajax({
+            type: "POST",
+            url: "https://westus.api.cognitive.microsoft.com/vision/v1.0/analyze?visualFeatures=Categories&language=en",
+            data: {
+                "url": "https://www.petmd.com/sites/default/files/what-does-it-mean-when-cat-wags-tail.jpg",
+            },
+            success: function(result) {
+                alert('ok');
+            },
+            error: function(result) {
+                alert('error');
+            }
+        });
     });
 });
